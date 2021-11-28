b68ed14d39a70a73ff4f7c470e9cfccfbe7d9038
Jeyi Adole <erthnice@gmail.com>
2021-11-28 15:44:45 +0100

    Progress

--- canvas-drawing-app/script.js
+++ canvas-drawing-app/script.js
@@ -1,9 +1,10 @@
 const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth - 60;
 canvas.height = 400;
+const colorFields = document.querySelectorAll('.color-field');
 
-let context = canvas.getContext('2d');
-context.fillStyle = 'white';
+let context = canvas.getContext('2d'); // Two dimesion context. Explain?
+context.fillStyle = 'white'; 
 context.fillRect(0, 0, canvas.width, canvas.height);
 
 let drawColor = 'black',
@@ -16,7 +17,6 @@ function start(e) {
     context.beginPath(); // Prepare canvas to do something
     context.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop) // Coordinates from event
     e.preventDefault();
-
 }
 
 function draw(e) {
@@ -28,14 +28,60 @@ function draw(e) {
         context.lineJoin = 'round';
         context.stroke();
     }
+    e.preventDefault();
 }
+
+function stop(e) {
+    if(isDrawing) {
+        context.stroke();
+        context.closePath();
+        isDrawing = false; 
+    }
+
+    e.preventDefault();
+} 
+
+function changeColor(element) {
+        drawColor = element.style.backgroundColor;
+    }
+    
+    for(let i = 0; i < colorFields.length; i++) {
+        colorFields[i].setAttribute("onclick", "changeColor(this)");
+    }
 
 canvas.addEventListener('touchstart', start, false);
 canvas.addEventListener('touchmove', draw, false);
 canvas.addEventListener('mousedown', start, false);
 canvas.addEventListener('mousemove', draw, false);
+canvas.addEventListener('touchend', stop, false);
+canvas.addEventListener('mouseup', stop, false);
+canvas.addEventListener('mouseout', stop, false);
 
 
 
 
 
+// PERSONAL EXPLANATION FOR THE FALSE KEYWORD IN canvas  '.addEventListener('touchstart', start, false);'
+// Event bubbling and event capturing 
+    // Bubbling
+        // When an event happens on an element the event handlers will first run on the element, then the elements parents and up
+
+    // Capturing 
+        // Bubbling vice versa, from the parent to the children and childrens' children.  
+// .-. false or true on the third element is for Usecapture where the event handler will be handled as bubbling if false and capture if true. Why do we need this?
+
+
+// Trial
+// function changeColor(element) {
+//     drawColor = element.style.backgroundColor;
+// }
+
+// for(let i = 0; i < colorFields.length; i++) {
+//     colorFields[i].setAttribute("onclick", "changeColor(this)");
+// }
+// colorFields.forEach(colorField => {
+//     colorField.addEventListener('click', () => {
+//         colorField.setAttribute("onclick", "clrfColor(this)");
+//     })
+// })
+// drawWidth = clrfColor();
